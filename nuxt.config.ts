7cdbb3717046d26540d8a99839d701b0d32215e2
Matt Bevilacqua <thewatermethod@gmail.com>
2023-06-19 16:43:07 -0400

    remove supabase plugin from nuxt stack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/supabase"],
+  // modules: ["@nuxt/content", "@nuxtjs/supabase"],
+  modules: ["@nuxt/content"],
   css: ["@/assets/main.css"],
 });
